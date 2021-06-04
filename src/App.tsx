cc08846b7f39f738b51677b0625e9fc4bdcc6ff6
chenwl <13751840799@163.com>
2021-06-05 03:19:55 +0800

    fix: delete console

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import { FullPageErrorFallback, FullPageLoading } from "components/lib";
 const AuthenticatedApp = React.lazy(() => import("./authenticated-app"));
 const UnauthenticatedApp = React.lazy(() => import("./unauthenticated-app"));
 
-console.log("===",process.env)
-
 function App() {
   const { user } = useAuth();
 
